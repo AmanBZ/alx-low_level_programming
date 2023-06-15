4023d085f3d92a6f4ba0e795a9c532cb3bcde665
AmanBZ <amanbelay09@gmail.com>
2023-06-15 11:23:36 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+ 	 Printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")std::err;
+	 return (0);
+}
