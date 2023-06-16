bd7d13b6c3a99828e5b94f70e7b1d2b2953736a1
AmanBZ <amanbelay09@gmail.com>
2023-06-16 05:27:26 -0700

    General Commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters except q and e
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a = 0;
+	while (a <= 10)
+		putchar(a);
+
+	return (0);
+}
