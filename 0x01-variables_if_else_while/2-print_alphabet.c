33c49fc88f94ce22e90094c2f748579fa3b764a6
AmanBZ <amanbelay09@gmail.com>
2023-06-16 04:55:48 -0700

    General Commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char g = 'a';
+
+	while (g <= z)
+	{
+		putchar(g);
+		g = g + 1;
+	}
+	putchar("\n");
+
+	return (0);
