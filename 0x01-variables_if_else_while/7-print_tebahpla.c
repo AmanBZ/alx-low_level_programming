41ebdaa6b6976c72b3af0d5d846801bf30aa96c0
AmanBZ <amanbelay09@gmail.com>
2023-06-16 09:14:05 -0700

    General Commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print all aplhabet letters
+ * Description: print a-z in reverse
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        char g = 'z';
-	
-	while (g >= 'a')
-        {
-                putchar(g);
-                --g;
-        }
-        putchar('\n');
+	char ch = 'z';
 
-        return (0);
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		--ch;
+	}
+	putchar('\n');
+
+	return (0);
 }
