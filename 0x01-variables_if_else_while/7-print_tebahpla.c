78d12d5331e5ea436ce5cc26de8d5f9aaf0810cb
AmanBZ <amanbelay09@gmail.com>
2023-06-16 09:06:36 -0700

    General Commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        char g = 'z';
+
+        while (g <= 'a')
+        {
+                putchar(g);
+                g = g - 1;
+        }
+        putchar('\n');
+
+        return (0);
+}i
