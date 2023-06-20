7aecf6c905e882d374f224db5a17d612e39133fe
AmanBZ <amanbelay09@gmail.com>
2023-06-20 10:24:36 -0700

    General Commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - function that will print the alphabet 10 times
  *
  * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
 {
-	int A = 'a', B;
+	int A = 1, B;
 
 	while (A <= 10)
 	{
