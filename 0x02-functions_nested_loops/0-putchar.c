c0d90f6c5d9237b59069571962cd9433b5f5729f
AmanBZ <amanbelay09@gmail.com>
2023-06-20 05:20:13 -0700

    no 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{ 
+
+	char a[10] = "_putchar";
+	printf("%s", a);
+
+	return (0);
+}
