fc1433b58d62aa81b4d8e2d87941a9f3af91ce44
AmanBZ <amanbelay09@gmail.com>
2023-06-20 12:20:48 -0700

    General Commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,41 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * print_times_table - prints out the n times table, starting with 0
- * @n: times table
- */
+ * print_times_table - prints time table of n
+ *
+ * @n: takes number input
+*/
+
 void print_times_table(int n)
 {
-int x, y, z, k, t;
+	int prod, mult, num;
 
-	for (x = 0; x <= n; x++)
+	if (n <= 15 && n >= 0)
 	{
-		for (y = 0; y <= n; y++)
+		for (num = 0; num <= n; ++num)
 		{
-			z = x * y;
-			if (z == 0 && y == 0)
-				_putchar(z + '0');
-			else if (z > 99)
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
 			{
-				k = z % 10;
-				t = (z / 10) % 10;
-				z /= 100;
 				_putchar(',');
 				_putchar(' ');
-				_putchar(z + '0');
-				_putchar(t + '0');
-				_putchar(k + '0');
-			}
-			else if (z > 9)
-			{
-				k = z % 10;
-				z /= 10;
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-				_putchar(k + '0');
 
+				prod = num * mult;
+
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				} else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
