c2fb7d656b2fa736856259d046ddf137fcdfcddf
AmanBZ <amanbelay09@gmail.com>
2023-06-20 10:18:58 -0700

    General Commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int A = 'a', B;
+
+	while (A <= 'z')
+	{
+		B = 'a';
+
+		while (B <= 'z')
+		{
+			_putchar(B);
+			B = B + 1;
+		}
+
+		_putchar('\n');
+		A = A + 1;
+
+	}
+
+
+}
