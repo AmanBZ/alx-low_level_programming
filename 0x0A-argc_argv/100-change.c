3e4a213519c44fe1dfc2fb7e46411dfe9359cfe7
AmanBZ <amanbelay09@gmail.com>
2023-07-07 11:57:06 -0700

    General Commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,61 +1,54 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#include <stdlib.h>
 
 /**
- * isNum - check if string array is num
- * @num: string to check
- * Return: 0 if it's a number
- *         1 if it's not a number
-*/
-
-int isNum(char num[])
-{
-	int i, l = strlen(num);
-
-	for (i = 0; i < l; i++)
-	{
-		if (!isdigit(num[i]))
-			return (1);
-	}
-	return (0);
-}
-
-
-/**
- * main - a program that adds positive numbers
+ * main - Prints the minimum number of coins to
+ *        make change for an amount of money.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * @argc: holds the number of arguments passed
- * @argv: array pointer that holds the arguments passed
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: If the number of arguments is not exactly one - 1.
+ *         Otherwise - 0.
+ */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int cents, coins = 0;
 
-	if (argc == 1)
+	if (argc != 2)
 	{
-		printf("0\n");
+		printf("Error\n");
+		return (1);
 	}
-	else
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
 	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
+		coins++;
+		if ((cents - 25) >= 0)
 		{
-			if (isNum(argv[i]) == 0)
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			cents -= 25;
+			continue;
 		}
-		printf("%d\n", sum);
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
 	}
+
+	printf("%d\n", coins);
+
 	return (0);
 }
